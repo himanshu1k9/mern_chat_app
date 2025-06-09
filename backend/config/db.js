8b3141baf6a24c35c050e89d5fb2224f0aceb467
himanshu1k9 <himanshu.develop.24@gmail.com>
2025-06-09 20:25:44 +0530

    Created basic setups for my app , folder structures and in backend connected with mongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () => 
+{
+    try 
+    {
+        await mongoose.connect(`${process.envMONGO_URL}/${process.env.MONGO_DB}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(() => {
+            console.log(`Mongo DB Connected successfully.`);
+        });
+    } catch(error) {
+        console.error(`MongoDB Coneection error :: ${error.message}`);
+    }
+}
+
+module.exports = connectDB;
---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: [true, 'Provided username already exists.'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'email field is required.'],
+        unique: [true, 'provided email is already exists.'],
+        lowercase: [true, 'email shuld be in lowercase.']
+    },
+    password: {
+        type: String,
+        minlength: [6, 'Password should not less than 6 characters.'],
+        maxlength: [10, 'Password should not more than 10 characters.']
+    }
+}, { timestamps: true } );
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    try
+    {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch(error) {
+        next(error);
+    }
+});
+
+const model = mongoose.model('User', userSchema);
+module.exports = model;
+
